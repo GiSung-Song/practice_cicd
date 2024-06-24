c9c57c1cdbca73d557fa31539ad6e40e3764bc4a
Song-Gi-Sung <wlswlek123@naver.com>
2024-06-24 17:39:11 +0900

    doing cd

--- src/main/java/practice/docker/sample/SampleController.java
+++ src/main/java/practice/docker/sample/SampleController.java
@@ -10,4 +10,9 @@ public class SampleController {
     public String getMain() {
         return "This is Main Page !!";
     }
+
+    @GetMapping("/sample")
+    public String getSample() {
+        return "This is Sample Page !!";
+    }
 }
